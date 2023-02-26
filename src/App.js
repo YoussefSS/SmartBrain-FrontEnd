ac972fc94e731d81dead1146ea8265ff2272fb9f
YoussefSS <joesamir98@gmail.com>
2023-02-26 17:21:21 +0200

    Adding the face detection box

--- src/App.js
+++ src/App.js
@@ -19,7 +19,28 @@ class App extends React.Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    console.log(data.outputs[0].data.regions[0].region_info.bounding_box);
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box})
   }
 
   onInputChange = (event) => {
@@ -37,7 +58,7 @@ class App extends React.Component {
       this.state.input // You have to use input here, not imageUrl .. why?
     )
     .then((response) => {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      this.displayFaceBox(this.calculateFaceLocation(response));
     })
     .catch((err) => {
       console.log(err);
@@ -55,7 +76,7 @@ class App extends React.Component {
         <ImageLinkForm 
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './FaceRecognition.css'
 
 const FaceRecognition = (props) => {
+    const box = props.box;
+    if(props.imageUrl==='')
+    {
+        return(<div></div>);
+    }
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
-             <img alt='faceimage' src={props.imageUrl} width='500px' height='auto'/>
+             <img id='inputimage' alt='faceimage' src={props.imageUrl} width='500px' height='auto'/> {/* inputimage will be found using document.getElementById in App.js */}
+             <div className='bounding-box' style={{top:box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
     )
